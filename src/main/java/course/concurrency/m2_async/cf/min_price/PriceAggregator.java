a5bab7d7e65b4d4bfff7a285e4263f50badf9a44
PAVEL KULAKOV <pavloskulakov@yandex.ru>
2023-10-14 10:47:03 +0300

    рефакторинг метода

--- src/main/java/course/concurrency/m2_async/cf/min_price/PriceAggregator.java
+++ src/main/java/course/concurrency/m2_async/cf/min_price/PriceAggregator.java
@@ -1,21 +1,17 @@
 package course.concurrency.m2_async.cf.min_price;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
+import java.util.concurrent.Executors;
 import java.util.stream.Collectors;
 
-import static java.util.concurrent.TimeUnit.*;
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
 
 public class PriceAggregator {
 
-    private static final int TIME_OUT = 2995;
+    private static final int TIME_OUT = 2950;
     private PriceRetriever priceRetriever = new PriceRetriever();
 
     public void setPriceRetriever(PriceRetriever priceRetriever) {
@@ -28,20 +24,16 @@ public class PriceAggregator {
 
     public double getMinPrice(long itemId) {
         // place for your code
-        long start = System.currentTimeMillis();
+        var executor = Executors.newFixedThreadPool(shopIds.size());
         List<CompletableFuture<Double>> futures = shopIds.stream()
                 .map(shopId -> CompletableFuture
-                                .supplyAsync(() -> priceRetriever.getPrice(itemId, shopId))
+                                .supplyAsync(() -> priceRetriever.getPrice(itemId, shopId), executor)
                                 .completeOnTimeout(null, TIME_OUT, MILLISECONDS)
-//                                .handle((res, ex) -> Double.NaN)
+                                .handle((res, ex) -> res == null ? Double.NaN: res)
                 )
                 .collect(Collectors.toList());
 
-        CompletableFuture<Void> future = CompletableFuture.allOf(futures.toArray(CompletableFuture[]::new));
-        future.join();
-
         return futures.stream()
-                .filter(f -> f.getNow(null) != null)
                 .map(CompletableFuture::join)
                 .min(Double::compare)
                 .orElse(Double.NaN);
