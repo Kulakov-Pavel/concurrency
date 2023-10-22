4befc02e0c4824fab9a1ac954a25b9e116af9920
PAVEL KULAKOV <pavloskulakov@yandex.ru>
2023-10-22 10:45:11 +0300

    refactoring ping pong

--- src/main/java/course/concurrency/m3_shared/PingPong.java
+++ src/main/java/course/concurrency/m3_shared/PingPong.java
@@ -8,40 +8,48 @@ public class PingPong {
     private final static Class<PingPong> lock = PingPong.class;
     private static volatile Value value;
 
+    static {
+        value = PANG;
+    }
+
     public static void ping() {
         for (; ; ) {
-            if (value == PANG || null == value) {
-                synchronized (lock) {
-                    sleep(1);
-                    System.out.println(PING.value);
-                    value = PING;
+            synchronized (lock) {
+                while (value != PANG) {
+                    waitThread();
                 }
+                sleep(1);
+                System.out.println(PING.value);
+                value = PING;
+                lock.notifyAll();
             }
         }
     }
 
     public static void pong() {
-
         for (; ; ) {
-            if (value == PING) {
-                synchronized (lock) {
-                    sleep(1);
-                    System.out.println(PONG.value);
-                    value = PONG;
+            synchronized (lock) {
+                while (value != PING) {
+                    waitThread();
                 }
+                sleep(1);
+                System.out.println(PONG.value);
+                value = PONG;
+                lock.notifyAll();
             }
         }
     }
 
     public static void pang() {
-
         for (; ; ) {
-            if (value == PONG) {
-                synchronized (lock) {
-                    sleep(1);
-                    System.out.println(PANG.value);
-                    value = PANG;
+            synchronized (lock ) {
+                while (value != PONG) {
+                    waitThread();
                 }
+                sleep(1);
+                System.out.println(PANG.value);
+                value = PANG;
+                lock.notifyAll();
             }
         }
     }
@@ -66,8 +74,8 @@ public class PingPong {
         Thread t1 = new Thread(PingPong::ping);
         Thread t2 = new Thread(PingPong::pong);
         Thread t3 = new Thread(PingPong::pang);
-        t3.start();
         t2.start();
+        t3.start();
         t1.start();
     }
 
