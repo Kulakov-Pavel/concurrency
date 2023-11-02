e0871548f04e6d8cf6030165f6106ee6b1e3f7af
PAVEL KULAKOV <pavloskulakov@yandex.ru>
2023-11-02 13:19:59 +0300

    memory bugfix

--- src/main/java/course/concurrency/exams/auction/Notifier.java
+++ src/main/java/course/concurrency/exams/auction/Notifier.java
@@ -1,20 +1,23 @@
 package course.concurrency.exams.auction;
 
-import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Notifier {
 
+    private final ExecutorService executor = Executors.newFixedThreadPool(64);
+
     public synchronized void sendOutdatedMessage(Bid bid) {
-        imitateSending();
+        executor.submit(this::imitateSending);
     }
 
     private void imitateSending() {
-        CompletableFuture.runAsync(() -> {
-            try {
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {}
-        });
+        try {
+            Thread.sleep(2);
+        } catch (InterruptedException ignored) {}
     }
 
-    public void shutdown() {}
+    public void shutdown() {
+        executor.shutdownNow();
+    }
 }
