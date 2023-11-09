94ef25d193a2d9cc9b379aab3721e60345761051
PAVEL KULAKOV <pavloskulakov@yandex.ru>
2023-11-09 21:10:54 +0300

    Restaurant service longAdder realization

--- src/main/java/course/concurrency/m3_shared/collections/RestaurantService.java
+++ src/main/java/course/concurrency/m3_shared/collections/RestaurantService.java
@@ -3,6 +3,7 @@ package course.concurrency.m3_shared.collections;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.LongAdder;
 import java.util.stream.Collectors;
 
 public class RestaurantService {
@@ -13,7 +14,7 @@ public class RestaurantService {
         put("C", new Restaurant("C"));
     }};
 
-    private final Map<String, Integer> stat = new ConcurrentHashMap<>();
+    private final Map<String, LongAdder> stat = new ConcurrentHashMap<>();
 
     public Restaurant getByName(String restaurantName) {
         addToStat(restaurantName);
@@ -21,12 +22,19 @@ public class RestaurantService {
     }
 
     public void addToStat(String restaurantName) {
-        stat.merge(restaurantName, 1, Integer::sum);
+        stat.compute(restaurantName, (k, v) -> {
+            if(v == null) {
+                v = new LongAdder();
+            }
+            v.increment();
+            return v;
+        });
+
     }
 
     public Set<String> printStat() {
         return stat.entrySet().stream()
-                .map(e -> String.format("%s - %s", e.getKey(), e.getValue()))
+                .map(e -> String.format("%s - %s", e.getKey(), e.getValue().sum()))
                 .collect(Collectors.toUnmodifiableSet());
     }
 }
