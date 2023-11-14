b6327860e799f04cc7a4a3caeea02fe0fdbdbd4f
PAVEL KULAKOV <pavloskulakov@yandex.ru>
2023-11-14 12:27:10 +0300

    Counter refactoring

--- src/main/java/course/concurrency/Counter.java
+++ src/main/java/course/concurrency/Counter.java
@@ -8,20 +8,39 @@ import java.util.stream.Stream;
 
 public class Counter {
     private static final Random RANDOM = new Random();
-    private static final int LIMIT = 1_000_000;
+    private static final int LIMIT = 10_000_000;
 
     public static void main(String[] args) {
+        long start, end;
         var pool = ForkJoinPool.commonPool();
         var list = Stream.generate(() -> RANDOM.nextInt(0, 1000))
                 .limit(LIMIT)
                 .map(String::valueOf)
                 .toList();
         int searchValue = 50;
-        int threshold = list.size() / (Runtime.getRuntime().availableProcessors());
+        int threshold = (int) (list.size() / (Runtime.getRuntime().availableProcessors() * 0.25));
+
+        start = System.currentTimeMillis();
+        int counter = CounterTask.count(list, 0, LIMIT, searchValue);
+        end = System.currentTimeMillis();
+        System.out.printf("""
+                        Последовательная обработка.
+                        В данном списке число %s встречается %d раз
+                        Время выполнения: %dмс.
+                        """,
+                searchValue, counter, end - start);
+
 
         CounterTask task = new CounterTask(list, 0, LIMIT, searchValue, threshold);
+        start = System.currentTimeMillis();
         Integer result = pool.invoke(task);
-        System.out.printf("В данном списке число %s встречается %d раз", searchValue, result);
+        end = System.currentTimeMillis();
+        System.out.printf("""
+                        Параллельная обработка.
+                        В данном списке число %s встречается %d раз
+                        Время выполнения: %dмс.
+                        """,
+                searchValue, result, end - start);
     }
 
     public static class CounterTask extends RecursiveTask<Integer> {
@@ -42,7 +61,7 @@ public class Counter {
         @Override
         protected Integer compute() {
             if (right - left <= threshold) {
-                return count();
+                return count(list, left, right, searchValue);
             } else {
                 int median = (right - left) / 2;
                 var a = new CounterTask(list, left, median, searchValue, threshold);
@@ -52,7 +71,7 @@ public class Counter {
             }
         }
 
-        private int count() {
+        public static int count(List<String> list, int left, int right, int searchValue) {
             int counter = 0;
             for (int i = left; i < right; i++) {
                 if (Integer.valueOf(list.get(i)).equals(searchValue))
